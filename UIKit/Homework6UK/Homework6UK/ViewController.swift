49bea00234e4a92d383fd6d876c218a942fbee97
RuslanWorkGit <lulka758@gmail.com>
2024-12-06 14:05:51 +0200

    end with MainImage View

--- UIKit/Homework6UK/Homework6UK/ViewController.swift
+++ UIKit/Homework6UK/Homework6UK/ViewController.swift
@@ -9,10 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dicountImage: UIImageView!
     @IBOutlet weak var mainImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mainImage.image = UIImage(named: "MotherBoard")
+        
+        dicountImage.layer.cornerRadius = dicountImage.frame.height / 2
+        dicountImage.image = UIImage(named: "FreeSend")
     }
 
 
