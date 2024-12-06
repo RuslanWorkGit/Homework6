7d03f82d81f1d7cad13c0213e40f24447e8b1eca
RuslanWorkGit <lulka758@gmail.com>
2024-12-06 14:21:50 +0200

    - start working with Main Info View
    - Add 2 lables for name and product code

--- UIKit/Homework6UK/Homework6UK/ViewController.swift
+++ UIKit/Homework6UK/Homework6UK/ViewController.swift
@@ -11,6 +11,12 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var dicountImage: UIImageView!
     @IBOutlet weak var mainImage: UIImageView!
+    
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productCodeLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -18,6 +24,9 @@ class ViewController: UIViewController {
         
         dicountImage.layer.cornerRadius = dicountImage.frame.height / 2
         dicountImage.image = UIImage(named: "FreeSend")
+        
+        productNameLabel.text = "Материнська плата Asus TUF Gaming B550M-Plus (sAM4, AMD B550, PCI-Ex16)"
+        productCodeLabel.text = "Код 218525893"
     }
 
 
