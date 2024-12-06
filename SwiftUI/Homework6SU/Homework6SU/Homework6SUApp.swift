7a8d0bf8f0216a81c65d572a8500f5b0af616edc
RuslanWorkGit <lulka758@gmail.com>
2024-12-06 12:36:55 +0200

    create empty projects
    - create empty SwiftUI and UIKit projects

---
+++ SwiftUI/Homework6SU/Homework6SU/Homework6SUApp.swift
@@ -0,0 +1,17 @@
+//
+//  Homework6SUApp.swift
+//  Homework6SU
+//
+//  Created by Ruslan Liulka on 06.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Homework6SUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
