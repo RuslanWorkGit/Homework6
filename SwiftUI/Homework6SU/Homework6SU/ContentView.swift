2e880e9174a7e2e5b0f0cf035d09b194927b624c
RuslanWorkGit <lulka758@gmail.com>
2024-12-09 18:56:33 +0200

    - add main image View

--- SwiftUI/Homework6SU/Homework6SU/ContentView.swift
+++ SwiftUI/Homework6SU/Homework6SU/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            MainImage()
+            TabView()
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
         }
         .padding()
     }
